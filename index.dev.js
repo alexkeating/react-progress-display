3d0a9845dccbaf72f5d2a83654cbbb6d3df94caa
Alexander Keating <atk315315@gmail.com>
2017-06-10 17:33:26 -0400

    Setup foundation for react component
    This commit sets up the foundation for a react component.
    It sets up webpack, eslint, and a basic component that renders Hi.

---
+++ index.dev.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import { ProgressBar } from './src/ProgressBar';
+
+render(
+  <ProgressBar />,
+  document.getElementById('root')
+);
---
+++ src/ProgressBar.js
@@ -0,0 +1,18 @@
+/**
+ * Created by alexkeating on 6/6/17.
+ */
+
+// What will this progress bar do?
+// 0.1.0 should be a straight line that can be partitioned.
+
+import React from 'react';
+
+const ProgressBar = () => {
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  );
+};
+
+export { ProgressBar };
